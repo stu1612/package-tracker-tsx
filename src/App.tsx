ff6fbbcf390c1b3e7b479c449d9d979d3489a7a4
Stu Bolderson <stu.bolderson@aol.com>
2022-08-28 14:47:36 +0200

    init setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// npm
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+// files
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Package from "./pages/Package";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/package" element={<Package />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
