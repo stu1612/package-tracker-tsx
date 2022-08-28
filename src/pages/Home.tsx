8253c0d1a88d481f5086673fcb4b7394d3568b45
Stu Bolderson <stu.bolderson@aol.com>
2022-08-28 17:26:46 +0200

    useFetch hook conditional messages returning correctly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,5 +5,11 @@ export default function Home() {
   const { data, loading, error } = useFetch(
     "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840"
   );
-  return <div>Home</div>;
+  return (
+    <div>
+      {loading && <p>Loading ...</p>}
+      {data && <p>data has succesfully been fetched!</p>}
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
